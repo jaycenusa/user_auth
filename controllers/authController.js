c6910475b25ff7caa6aacab6aa53a5e55bb9ebc5
Jay C <jaycenusa@gmail.com>
2025-11-28 11:51:18 -0500

    feat: add jwt access and refresh tokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,16 @@ const usersDB = {
 }
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
+    console.log('user:', user, 'pwd:', pwd);
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
     const foundUser = usersDB.users.find(person => person.username === user);
     if (!foundUser) return res.sendStatus(401); //Unauthorized 
@@ -13,7 +21,37 @@ const handleLogin = async (req, res) => {
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
         // create JWTs
-        res.json({ 'success': `User ${user} is logged in!` });
+        const accessToken = jwt.sign(
+            {"username": foundUser.username}, 
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        const refreshToken = jwt.sign(
+            {"username": foundUser.username}, 
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+
+        // find other users except the logged in user
+        const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
+
+        const currentUser = { ...foundUser, refreshToken };
+
+        // update the users array
+        usersDB.setUsers([...otherUsers, currentUser]);
+
+        usersDB.setUsers([...usersDB.users]);
+
+        // save the updated users array to the json file
+        await fsPromises.writeFile(
+            path.join(__dirname, '..', 'model', 'users.json'),
+            JSON.stringify(usersDB.users)
+        )
+
+        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 }); //1 day
+
+        res.json({ accessToken });
     } else {
         res.sendStatus(401);
     }
